6286a01d696cbcf0287de29fc3ab9400f0404710
Yauheni Shypila <evshipilo@gmail.com>
2020-03-03 18:05:41 +0300

    add: eval function

--- src/index.js
+++ src/index.js
@@ -3,45 +3,47 @@ function eval(str) {
     // строку str.replace()
     // снова ищем и подставляем
     while (true) {
-        if (str.search(/(\d+ \/ \d+)|(\d+ \* \d+)/) == -1 &&
-            str.search(/(\d+ \+ \d+)|(\d+ \- \d+)/) == -1) break;
+        if (str.search(/(\-*\d+ \/ \-*\d+)|(\-*\d+ \* \-*\d+)/) == -1 &&
+            str.search(/(\-*\d+ \+ \-*\d+)|(\-*\d+ \- \-*\d+)/) == -1) break;
 
-        if (str.search(/(\d+ \/ \d+)|(\d+ \* \d+)/) != -1) {
+        if (str.search(/(\-*\d+ \/ \-*\d+)|(\-*\d+ \* \-*\d+)/) != -1) {
 
-            str = str.replace(/(\d+ \/ \d+)|(\d+ \* \d+)/, function (match) {  //выдаст первое совпадение match или с / или с *
-                if (match.search(/(\d+ \/ \d+)/) != -1) {
+            str = str.replace(/(\-*\d+ \/ \-*\d+)|(\-*\d+ \* \-*\d+)/, function (match) {  //выдаст первое совпадение match или с / или с *
+                if (match.search(/(\-*\d+ \/ \-*\d+)/) != -1) {
                     let arr = match.split(' / ');
-                    return (+arr[0] / +arr[1]).toFixed(4);
+                    return ((+arr[0]) / (+arr[1])+'');   //доп скобки для
+                    // учета отрицательных
                 } else {
                     let arr = match.split(' * ');
-                    return (+arr[0] * +arr[1]).toFixed(4);
+                    return ((+arr[0]) * (+arr[1])+'');
                 }
             })
         }
 
 
-        if (str.search(/(\d+ \/ \d+)|(\d+ \* \d+)/) == -1 &&
-            str.search(/(\d+ \+ \d+)|(\d+ \- \d+)/) != -1) {
+        if (str.search(/(\-*\d+ \/ \-*\d+)|(\-*\d+ \* \-*\d+)/) == -1 &&
+            str.search(/(\-*\d+ \+ \-*\d+)|(\-*\d+ \- \-*\d+)/) != -1) {
 
-            str = str.replace(/(\d+ \+ \d+)|(\d+ \- \d+)/, function (match) {  //выдаст первое совпадение match или с / или с *
-                if (match.search(/(\d+ \+ \d+)/) != -1) {
+            str = str.replace(/(\-*\d+ \+ \-*\d+)|(\-*\d+ \- \-*\d+)/, function (match) {  //выдаст первое совпадение match или с / или с *
+                if (match.search(/(\-*\d+ \+ \-*\d+)/) != -1) {
                     let arr = match.split(' + ');
-                    return (+arr[0] + +arr[1]).toString();
+                    return ((+arr[0]) + (+arr[1])).toString();
                 } else {
-                    let arr = match.split(' * ');
-                    return (+arr[0] * +arr[1]).toString();
+                    let arr = match.split(' - ');
+                    return ((+arr[0]) - (+arr[1])).toString();
                 }
             })
         }
 
-        return;
     }
+    return str
+}
 
 
-    function expressionCalculator(expr) {
+function expressionCalculator(expr) {
         // ищем выражение в скобках и вызываем eval, подставляем значение вместо
         // скобок
-
+    if (str.search(/\([^\(\)]*\)/) != -1)
 
     }
 
